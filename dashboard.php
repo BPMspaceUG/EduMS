bb017f9ca9ec2c828ad53022df1f71027221156a
rootmessages BPMspace <rootmessages@bpmspace.com>
2016-01-17 08:14:49 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,9 @@ include_once '_header.inc.php';
  <i class="fa fa-tachometer fa-5x text-success"></i>
  <i class="fa fa-tachometer fa-5x text-danger"></i>
 </div> 
-<p> example <a href="http://bootstrapmaster.com/live/perfectum/" > Dashboard Example</a>
+<p> example </br>
+<a href="http://bootstrapmaster.com/live/perfectum/" > Dashboard Example</a>
+<a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin/index-rtl.html  > Dashboard Example II/a>
 <?php
 include_once '_footer.inc.php';
 ?>
