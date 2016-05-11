b28bb2765c4c3fa3773cdcd6244bea68fbfe8cc0
cnu301 <cnu301@mitsm.de>
2016-05-11 21:53:37 +0200

    add sample code for phpmailer

--- api/RequestHandler.inc.php
+++ api/RequestHandler.inc.php
@@ -126,6 +126,17 @@ class RequestHandler
                 return $return;
                 break;
 
+            case 'reserveform': 
+            // $handle[0];
+                $return = array(
+                'script'=>file_get_contents('js/Underscore v1.8.3.js').file_get_contents('js/jQuery 2.2.1.js').file_get_contents('js/AngularJS v1.4.9.js').file_get_contents('js/Bootstrap v3.3.6.js'),
+                'controller'=>"<script type=\"text/javascript\">var app = angular.module('application', ['ngSanitize']); </script>".file_get_contents('js/Form_Ctrl.js'),
+                'css'=>file_get_contents('css/3.3.6 bootstrap.min.css').file_get_contents('css/EduMS_custom.css').$this->usercss,
+                'directive'=>file_get_contents('js/EduMS_reserveForm_template_directives.js'),
+                'ct'=>file_get_contents('reserveform.html'));                
+                return $return;
+                break;
+
             //database getters
             case 'getTopics': $response = array('topiclist' => $this->vTopicNotdepercated(), 'topiccourseCourselist'  => $this->vTopiccourseNotdepercatedlevelnotzero(), 'eventcourselocationFuture'  => $this->vEventcourselocationFuturepublicnotdepercatednotstornonotnew());
                 return $response;
@@ -145,8 +156,65 @@ class RequestHandler
                 break;
 
             case "reserve":
-            require 'reservemail.php';
-            echo "reservemail aufgerufen.";
+//Muster url für reservierende person: http://localhost:4040/EduMS-client/index.php?navdest=reserveform&brand_id=1&course_id=74,174&participants=2  
+                // require_once '/PHPMailer/PHPMailerAutoload.php';
+                 
+                // $results_messages = array();
+                 
+                // $mail = new PHPMailer(true);
+                // $mail->CharSet = 'utf-8';
+                // ini_set('default_charset', 'UTF-8');
+                 
+                // class phpmailerAppException extends phpmailerException {}
+                 
+                // try {
+                // $to = 'cnu301@mitsm.de';
+                // if(!PHPMailer::validateAddress($to)) {
+                //   throw new phpmailerAppException("Email address " . $to . " is invalid -- aborting!");
+                // } 
+                // $mail->isSMTP();
+                // $mail->SMTPDebug  = 2;
+                // $mail->Host       = "smtp.office365.com";
+                // $mail->Port       = "587";
+                // $mail->SMTPSecure = "tls";
+                // $mail->SMTPAuth   = true;
+                // $mail->Username   = "cnu301@mitsm.de";
+                // $mail->Password   = "Passworrt";
+                // $mail->addReplyTo("cnu301@mitsm.de", "Cedric");
+                // $mail->setFrom("cnu301@mitsm.de", "Cedric");
+                // $mail->addAddress("cnu301@mitsm.de", "CedricN");
+                // $mail->addCC("cnu301@mitsm.de");
+                // $mail->Subject  = "Betrifft test (PHPMailer test using SMTP)";
+                // $body = <<<'EOT'
+                // Dies ist der Text
+                // EOT;
+                // $mail->WordWrap = 78;
+                // $mail->msgHTML($body, dirname(__FILE__), true); //Create message bodies and embed images
+                // $mail->addAttachment('images/phpmailer_mini.png','phpmailer_mini.png');  // optional name
+                // $mail->addAttachment('images/phpmailer.png', 'phpmailer.png');  // optional name
+                 
+                // try {
+                //   $mail->send();
+                //   $results_messages[] = "Message has been sent using SMTP";
+                // }
+                // catch (phpmailerException $e) {
+                //   throw new phpmailerAppException('Unable to send to: ' . $to. ': '.$e->getMessage());
+                // }
+                // }
+                // catch (phpmailerAppException $e) {
+                //   $results_messages[] = $e->errorMessage();
+                // }
+                 
+                // if (count($results_messages) > 0) {
+                //   echo "<h2>Run results</h2>\n";
+                //   echo "<ul>\n";
+                // foreach ($results_messages as $result) {
+                //   echo "<li>$result</li>\n";
+                // }
+                // echo "</ul>\n";
+                // }
+
+            
             // testmail();
             //     $to      = 'cnu301@mitsm.de';
             //     $subject = 'Reservierungsanfrage: Nachname Vorname, Courseabkürzung, Datum, Anzahl';
