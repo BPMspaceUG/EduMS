c02f98503695460429476ca51f32d1392400a828
cnu301 <cnu301@mitsm.de>
2017-03-28 11:09:27 +0200

    better dummyData and debug

--- mysql_dump/20170327_packages_structure_dummyData.sql
+++ mysql_dump/20170327_packages_structure_dummyData.sql
@@ -2,6 +2,7 @@
 DROP TABLE IF EXISTS `package`;
 CREATE TABLE IF NOT EXISTS `package` (
 `package_id` int(11) NOT NULL,
+  `brand_id` int(11) NOT NULL DEFAULT 0,
   `package_discount` float(8,2) NOT NULL DEFAULT 0.00,
   `packageName` longtext NOT NULL,
   `packageHeadline` tinytext NOT NULL,
@@ -13,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `package` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- Indizes für die Tabelle `package`
 ALTER TABLE `package`
- ADD PRIMARY KEY (`package_id`);
+ ADD PRIMARY KEY (`package_id`), ADD KEY `brand_id` (`brand_id`);
 -- AUTO_INCREMENT für Tabelle `package`
 ALTER TABLE `package`
  MODIFY `package_id` int(11) NOT NULL AUTO_INCREMENT;
@@ -39,35 +40,32 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 `package`.`package_discount` AS `package_discount`, `package`.`packageName` AS `packageName`, `package`.`packageHeadline` AS `packageHeadline`, 
 `package`.`packageDescription` AS `packageDescription`, `package`.`packageDescriptionSidebar` AS `packageDescriptionSidebar`, 
 `package`.`packageImage` AS `packageImage`, `package`.`packageDescriptionFooter` AS `packageDescriptionFooter`, 
-`package_course`.`course_id` AS `course_id`, 
+`package_course`.`course_id` AS `course_id`, `package`.`brand_id` AS `brand_id`, 
 ( select COUNT(`package_id`) from `package_course` pc where pc.`package_id` = `package_course`.`package_id`) AS `package_course_amount`, 
 `course`.`course_name` AS `course_name`, `package_course`.`level` AS `level`, `package_course`.`rank` AS `rank` 
 from (
-	(`bpmspace_edums_v4`.`package_course` `package_course` 
-		left join `bpmspace_edums_v4`.`course` `course` 
+	(`bpmspace_edums_v5`.`package_course` `package_course` 
+		left join `bpmspace_edums_v5`.`course` `course` 
 		on((`package_course`.`course_id` = `course`.`course_id`))
 	) 
-	left join `bpmspace_edums_v4`.`package` `package` 
+	left join `bpmspace_edums_v5`.`package` `package` 
 		on((`package_course`.`package_id` = `package`.`package_id`))
 )
 where ((`course`.`deprecated` <> '1') and (`package_course`.`level` <> '0'));
 
 
--- Demodata for package and package_course
-INSERT INTO `package` (`package_discount`, `packageName`, `packageHeadline`, `packageDescription`, 
-	`packageDescriptionSidebar`, `packageImage`, `packageDescriptionFooter`, `deprecated`) 
-VALUES
-(7.75, 'Foundation', 'package Foundation ID 1', '<p>package description: A package of all Foundation courses from different topics.</p>', 
-	'<p>package description sidebar: A package of all Foundation courses from different topics.</p>', 'no img.',
-	 '<p>package description footer: A package of all Foundation courses from different topics.</p>', 0),
-(7.75, 'Whole Topic', 'package Whole Topic ID 2', '<p>package description: A package of all courses from a topic.</p>', 
-	'<p>package description sidebar: A package of all courses from a topic.</p>', 'no img.',
-	 '<p>package description footer: A package of all courses from a topic.</p>', 0),
-(7.75, 'Three in Location', 'package three in Location ID 3', '<p>package description: A package of three couses at a location.</p>', 
-	'<p>package description sidebar A package of three couses at a location.</p>', 'no img.',
-	 '<p>package description footer: A package of three couses at a location.</p>', 0);
 
-INSERT INTO `package_course` ( `package_id`, `course_id`, `level`, `rank`) VALUES
-(1, 11, 1, 1), (1, 12, 1, 1), (1, 13, 1, 1),
-(2, 26, 1, 1), (2, 27, 2, 1), (2, 28, 3, 1),
-(3, 1, 1, 1), (3, 4, 2, 1), (3, 9, 2, 2), (3, 2, 3, 1);
+-- Demodata
+INSERT INTO `package` (`deprecated`) VALUES (0),(0),(0),(0),(0),(0),(0),(0),(0);
+UPDATE `package` SET `brand_id` = str_random('[1|2|3]');
+UPDATE `package` SET `package_discount` = concat(str_random('D'),'.',str_random('D'),str_random('D'));
+UPDATE `package` SET `packageName` = concat('packageName - ',`package_id`,' ',str_random_lipsum(1,1,NULL));
+UPDATE `package` SET `packageHeadline` = concat('packageHeadline - ',`package_id`,' ',str_random_lipsum(1,1,NULL));
+UPDATE `package` SET `packageDescription` = concat('<p>packageDescription - ',`package_id`,' ',str_random_lipsum(1,1,NULL),'</p>');
+UPDATE `package` SET `packageDescriptionSidebar` = concat('<p>packageDescriptionSidebar - ',`package_id`,' ',str_random_lipsum(1,1,NULL),'</p>');
+UPDATE `package` SET `packageImage` = concat('packageImage - noImg.');
+UPDATE `package` SET `packageDescriptionFooter` = concat('<p>packageDescriptionFooter - ',`package_id`,' ',str_random_lipsum(1,1,NULL),'</p>');
+
+INSERT INTO `package_course` ( `level`, `rank`) VALUES (1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1),(1,1);
+UPDATE `package_course` SET `package_id` = str_random('[1|2|3|4|5]');
+UPDATE `package_course` SET `course_id` = (SELECT `course_id` FROM `v_eventcourselocation_futurepublicnotdepercatednotstornonotnew` where `test` != 1 order by rand() limit 1);
